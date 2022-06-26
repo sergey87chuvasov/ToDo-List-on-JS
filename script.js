c5b534e627c485e261e067125c4e2558fa72d0d1
Sergey87 <serge.techno@gmail.com>
2022-06-26 22:36:16 +0300

    added change status of task

--- script.js
+++ script.js
@@ -28,6 +28,7 @@ function printTodo({ id, userId, title, completed }) {
   const status = document.createElement('input');
   status.type = 'checkbox';
   status.checked = completed;
+  status.addEventListener('change', handleTodoChange);
 
   const close = document.createElement('span');
   // html = x
@@ -72,6 +73,13 @@ function handleSubmit(event) {
   });
 }
 
+function handleTodoChange() {
+  const todoId = this.parentElement.dataset.id;
+  const completed = this.checked;
+
+  toggleTodoComplete(todoId, completed);
+}
+
 // Async logic
 // get all tasks, await use inside - not ouside
 async function getAllTodos() {
@@ -102,4 +110,21 @@ async function createTodo(todo) {
   const newTodo = await response.json();
 
   printTodo(newTodo);
+}
+
+async function toggleTodoComplete(todoId, completed) {
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/todos/${todoId}`,
+    {
+      method: 'PATCH',
+      body: JSON.stringify({ completed: completed }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    }
+  );
+
+  if (!response.ok) {
+    //errror
+  }
 }
